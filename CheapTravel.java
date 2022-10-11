e30e2900efece7374ab08aa911f1ab80f102b12d
Pablo <p.guatibonza@uniandes.edu.co>
2022-10-11 13:39:32 -0500

    cheapTravel

---
+++ CheapTravel.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CheapTravel {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+        int x=n/m;
+        int total=0;
+        if(b<m*a){
+            total=b*x;
+        }
+        else{
+            total=x*m*a;
+        }
+
+        if(b<a*(n-x*m)){
+            total+=b;
+        }
+        else{
+            total+=a*(n-x*m);
+        }
+        System.out.println(total);
+    }
+    
+}
